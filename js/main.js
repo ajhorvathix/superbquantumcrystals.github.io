a1a6be0541ff000ba21638ed9a0a61bb30ffb6d4
ajhorvathix <119686771+ajhorvathix@users.noreply.github.com>
2023-12-10 21:05:52 -0500

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+  $('.nojs').hide(); 
+
+  $(function() {
+    console.log("js is (t)werking");
+    $('#typedJS').typed({
+      strings: [
+      "Hello there",
+      "I'm Alex",
+      "check me out below.",
+      ],
+      typeSpeed: 60,
+      backSpeed: 30,
+      backDelay: 1400,
+      loop: true,
+    });
+  }); 
+
+});

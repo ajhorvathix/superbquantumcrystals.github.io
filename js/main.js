7fb9b5acfd01c11f3767ca3b642f0889dd7f1f6d
ajhorvathix <119686771+ajhorvathix@users.noreply.github.com>
2025-01-02 21:42:48 -0500

    Update main.js

--- js/main.js
+++ js/main.js
@@ -14,10 +14,18 @@ $(document).ready(function(){
       "What's for breakfast?",
       "Elderberry jam toast, and tea!",
       "What about second breakfast?",
-      "Oh, sandwiches with sweet nectar!"
-      "Excellent, and lunch, snack, supper, and dinner?"
-      "We'll go to the market and figure it out!"
+      "Oh, sandwiches with sweet nectar!",
+      "Excellent, and lunch, snack, supper, and dinner?",
+      "We'll go to the market and figure it out!",
       "Awesome I'm ready, let's eat!",
+      "...3 Days Later...",
+      "...",
+      "Wow, I'm about as stuffed as slaughter cow",
+      "Uuuuuugggghhhh...me too..",
+      "We should take a walk through the shire.",
+      "Yes to work off all this good food.",
+      "You're my best friend you know?",
+      "You too, friend.",
       ],
       typeSpeed: 60,
       backSpeed: 30,
